c7c66519993df517b3c554e7afd9cb097ce51c1e
Thinesan <thinesan@Thinesans-MBP-2.broadband>
2023-03-31 15:12:50 +0100

    fixed cast images

--- client/src/Components/Cast.js
+++ client/src/Components/Cast.js
@@ -1,6 +1,13 @@
 const Cast = ({cast,onDelete}) => {
+    function importAll(r) {
+        let images = {};
+        r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+        return images;
+      }
+      const pics = importAll(require.context('../pics/Cast', false, /\.(png|jpg|svg)$/));
     return (
         <div className="cast">
+            <img id="image" src={pics[`${cast.name}.jpg`]} alt={cast.name}/>
             <h3>{cast.name}</h3>
             <p>Actor's Age: {cast.age}</p>
             <p>Actor's Bio: {cast.bio}</p>

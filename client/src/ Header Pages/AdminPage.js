c959bfef968a7215afd760435636265514134814
Ramiro Richmand <ramirorichmand@Ramiros-Coding-MacBook-Pro.local>
2023-04-02 18:23:40 +0100

    rewrote admin page for easier css accessibility

--- client/src/ Header Pages/AdminPage.js
+++ client/src/ Header Pages/AdminPage.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const AdminPage = () => {
     return(
-    <>
-    <h2>Admin</h2>
-    <p>As an admin, you can easily manage the movie and cast database.</p>
-    <p>To add or remove movies from the database, simply click on the "Movie".</p>
-    <p>To add or remove cast members from the database, click on the "Cast".</p>
-    <Link to="/Admin/Movie">Movie</Link>
-    <Link to="/Admin/Cast">Casts</Link>
-    </>
+      <div className="admin-container">
+        <h2 className="admin-title">Welcome, Admin</h2>
+        <p className="admin-description">As an admin, you can easily manage the movie and cast database.</p>
+        <p className="admin-description">To add or remove movies from the database, simply click on the "Movie".</p>
+        <p className="admin-description">To add or remove cast members from the database, click on the "Cast".</p>
+        <Link to="/Admin/Movie" className="admin-link">Manage Movies</Link>
+        <Link to="/Admin/Cast" className="admin-link">Manage Casts</Link>
+      </div>
     )
 }
 

5d6bcf0e81fcd169329108f5b2c64ed916371738
ryder <ryderzrashid@gmail.com>
2023-04-04 15:46:26 +0100

    on-click title takes you to homepage

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,46 +1,47 @@
 import logo from './Logo.png';
 import video from './video.mp4'
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import BlockbusterRebornLogo from './BlockbusterRebornLogo.png';
 
-const Header = () => { 
+const Header = () => {
   return (
     <div className="header">
-  <div className="header-logo-container">
-  <img src="https://i.imgur.com/NzIiXNq.png" alt="Blockbuster.gif" height="50"/>
-  <img src={BlockbusterRebornLogo} alt="BlockbusterRebornLogo.png" height="55"/>
-  </div>
-
-  
-
-    {/* fix popcorn gif so it's on same level as blockbuster reborn logo */}
-
-  <div className="header-nav-container">
-    <div className="header-nav">
-      <Link to="/Home">Home</Link>
-      <Link to="/MyList">MyList</Link>
-      <Link to="/Login">Login</Link>
-      <Link to="/Admin">Admin</Link>
-      <Link to="/Subscribe">Subscribe</Link>
-      <div className="header-search">
-      <input type="search" placeholder="Search" />
+      <div className="header-logo-container">
+        <a href='http://localhost:3000/Home'>
+          <img src="https://i.imgur.com/NzIiXNq.png" alt="Blockbuster.gif" height="50" />
+          <img src={BlockbusterRebornLogo} alt="BlockbusterRebornLogo.png" height="55" />
+        </a>
       </div>
-      <div className="header-avatar-container">
-    <img
-      src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-      alt="Avatar"
-      height="75"
-    />
-  </div>
-    </div>
-    </div>
 
 
-</div>
+
+      {/* fix popcorn gif so it's on same level as blockbuster reborn logo */}
+
+      <div className="header-nav-container">
+        <div className="header-nav">
+          <Link to="/Home">Home</Link>
+          <Link to="/MyList">MyList</Link>
+          <Link to="/Login">Login</Link>
+          <Link to="/Admin">Admin</Link>
+          <Link to="/Subscribe">Subscribe</Link>
+          <div className="header-search">
+            <input type="search" placeholder="Search" />
+          </div>
+          <div className="header-avatar-container">
+            <img
+              src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+              alt="Avatar"
+              height="75"
+            />
+          </div>
+        </div>
+      </div>
+
+
+    </div>
 
   )
-    
+
 };
 
 export default Header;
-  

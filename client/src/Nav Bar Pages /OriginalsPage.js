adcc420422c7184012dc02433de64d477a1eeb56
Thinesan <thinesan@Thinesans-MBP-2.broadband>
2023-04-05 00:07:53 +0100

    added cast details for top rated movies

--- client/src/Nav Bar Pages /OriginalsPage.js
+++ client/src/Nav Bar Pages /OriginalsPage.js
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
 
-const OriginalsPage = () => {
+const OriginalsPage = ({}) => {
   function importAll(r) {
     let images = {};
     r.keys().map((item, index) => {
@@ -13,7 +13,8 @@ const OriginalsPage = () => {
     return images;
   }
   
-  const pics = importAll(require.context('../pics/Movie', false, /\.(png|jpg|svg)$/));
+const pics = importAll(require.context('../pics/Movie', false, /\.(png|jpg|svg)$/));
+
   
   const [movies, setMovies] = useState([]);
 
@@ -36,27 +37,49 @@ const OriginalsPage = () => {
     autoplaySpeed: 5000,
   }
 
-    // Trailer Button Function 
-    const [showTrailer, setShowTrailer] = useState(false); // initialize state to false
-    const handleTrailerVideo = () => {
-      setShowTrailer(!showTrailer); // toggle state on button click
-    }
+   // Trailer Button Function 
+   const [showTrailer, setShowTrailer] = useState(false); // initialize state to false
+   const handleTrailerVideo = () => {
+     setShowTrailer(!showTrailer); // toggle state on button click
+   }
+
+   // Movie Detail Button 
+   const [showMovieDetails, setShowMovieDetails] = useState(false);
+   const handleMovieDetails = () => {
+    setShowMovieDetails(!showMovieDetails); // toggle state on button click
+  }
 
   return (
     <div className="originals-container">
       <h2>Blockbuster Originals Movies</h2>
-      <br/>
       <Slider {...sliderSettings}>
         {movies.map(movie => (
           <div key={movie.id}>
             <center>
             <img id="image" src={pics[`${movie.title}.jpg`]} alt={movie.title} />
-            <h3>{movie.title}</h3>
+            <br/>
+            <h2>{movie.title}</h2>
+            <hr/>
+            {showMovieDetails && (
+              <div>
             <p>Movie Duration (mins): {movie.duration}</p>
             <p>Movie Review: {movie.review}</p>
             <p>Movie Rating: {movie.rating}</p>
             <p>Movie Language: {movie.language}</p>
             <p>Movie Genre: {movie.genre}</p>
+            <hr/>
+            <h3>Cast 1</h3>
+            <p> Name: {movie.movieCastMember[0].cast.name}</p>
+            <p> Age: {movie.movieCastMember[0].cast.age}</p>
+            <p>Actor's Bio: <br/>{movie.movieCastMember[0].cast.bio}</p>
+            <hr/>
+            <h3>Cast 2</h3>
+            {movie.movieCastMember.length >= 2 && <p>Name: {movie.movieCastMember[1].cast.name}</p>}
+            {movie.movieCastMember.length >= 2 && <p>Age: {movie.movieCastMember[1].cast.age}</p>}
+            {movie.movieCastMember.length >= 2 && <p>Bio: {movie.movieCastMember[1].cast.bio}</p>}
+            <hr/>
+            </div>
+            )}
             {showTrailer && (
             <iframe
               src={`${movie.trailer}`}
@@ -66,7 +89,12 @@ const OriginalsPage = () => {
               width="400"
             ></iframe>
           )}
-          <button onClick={handleTrailerVideo}>
+          <button onClick={handleMovieDetails}>
+            {showMovieDetails ? "Hide Movie Details" : "View Movie Details"}
+          </button>
+          <br/>
+          <br/>
+         <button onClick={handleTrailerVideo}>
             {showTrailer ? "Hide Trailer" : "Watch Trailer"}
           </button> 
           <Link to="/Login">
@@ -78,6 +106,7 @@ const OriginalsPage = () => {
       </Slider>
     </div>
   )
-}
+   
+};
 
 export default OriginalsPage;
